e62dddfd9363788c598410837656100e464ae442
A Rao <76902064+LilDerp-IsBetter@users.noreply.github.com>
2021-02-19 13:17:51 +0000

    Added Files from local machine

---
+++ src/lilpanda/autoupdater/AutoUpdater.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace lilpanda\autoupdater;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class AutoUpdater extends PluginBase{
+
+	public $config;
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents(new EventHandler($this), $this);
+
+		if(!is_dir($this->getDataFolder())){
+			mkdir($this->getDataFolder());
+		}
+		$this->saveResource("config.yml");
+
+		$this->config = new Config($this->getDataFolder() . "config.yml");
+
+		if($this->config->get("restartAfterUpdate") === false){
+			$this->config->set("restartAfterUpdate", false);
+			$this->config->save();
+		}
+	}
+}
